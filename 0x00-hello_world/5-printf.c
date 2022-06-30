f4cc456953006f22df01e789cf348cf8da2d3b8d
ZekariasBehailu <zeking02@gmail.com>
2022-06-30 07:12:25 -0700

    task 5-printf.c done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
