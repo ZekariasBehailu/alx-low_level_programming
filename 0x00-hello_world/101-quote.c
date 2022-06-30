fa022f36ec1e02c22dabb9cf30c0b1436dc534e0
ZekariasBehailu <zeking02@gmail.com>
2022-06-30 08:55:58 -0700

    task 101 done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 1 output to stderr
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
