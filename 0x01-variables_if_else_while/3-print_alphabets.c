eaad7835810060c94f7c59b231787df1725863ab
ZekariasBehailu <zeking02@gmail.com>
2022-07-01 06:49:07 -0700

    Task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
