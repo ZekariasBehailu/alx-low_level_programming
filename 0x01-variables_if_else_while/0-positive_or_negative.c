fa13715a4ae74aa45fd98ad5e6106f58fbba55c5
ZekariasBehailu <zeking02@gmail.com>
2022-07-01 05:34:35 -0700

    Code refactoring for task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - main function
  *
- * return: 0 success
+ * Return: 0 success
  */
 int main(void)
 {
@@ -12,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	if(n == 0)
+	if (n == 0)
 		printf("%d is zero\n", n);
-	if(n < 0)
+	if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
