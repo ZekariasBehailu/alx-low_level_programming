537da44c5753cb0c24e0fe1dc09c667467516e74
ZekariasBehailu <zeking02@gmail.com>
2022-07-01 06:58:21 -0700

    task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
