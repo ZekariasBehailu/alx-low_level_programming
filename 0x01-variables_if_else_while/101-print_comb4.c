5c40d5675c0f4a3c35767de0e53591f579dee82a
ZekariasBehailu <zeking02@gmail.com>
2022-07-01 10:14:33 -0700

    task 101 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = '0'; i <= '7'; i++)
+	{
+		for (j = i; j <= '8'; j++)
+		{
+			for(k = j; k <= '9'; k++)
+				if (i != j && i != k && j != k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i + j != 113)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
