b4026f6b87141bb9695be9159efd3c51ee0abb0a
ZekariasBehailu <zeking02@gmail.com>
2022-07-01 07:09:53 -0700

    Task 6 done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
