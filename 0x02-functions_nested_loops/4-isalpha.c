79067e7c016073a805909bf51f5b0a0c4db2425e
ZekariasBehailu <zeking02@gmail.com>
2022-07-05 14:42:33 +0300

    tast 4 done

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - idetifies if a character is an alphabet
+ * @c: The character to be identified
+ *
+ * Return: On success 1.
+ */
+
+int _isalpha(int c);
+{
+	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
