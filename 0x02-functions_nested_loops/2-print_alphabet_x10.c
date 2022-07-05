55a7e1083292849db63991aed5a04e8b3630ac4b
ZekariasBehailu <zeking02@gmail.com>
2022-07-05 13:40:09 +0300

    tast 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints small letter alphabets x10
+ *
+ */
+void print_alphabet_x10(void);
+{
+	char ch;
+	int n;
+
+	for (n = 0; n <= 10; n++)
+        for (ch = 'a'; ch <= 'z'; ch++)
+            _putchar(ch);
+	_putchar('\n');
+}
