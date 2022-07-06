0200cb001aa5825185b4460f30102b37e383abd5
ZekariasBehailu <zeking02@gmail.com>
2022-07-06 19:51:37 +0300

    Project Debugging

---
+++ .history/0x02-functions_nested_loops/6-abs_20220705152105.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - idetifies sign of a number
+ * @n: The character to be identified
+ *
+ * Return: On success 1.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
---
+++ .history/0x02-functions_nested_loops/7-print_last_digit_20220705161402.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - calculates absolute value of a numnet
+ * @ab: number to be calculated
+ *
+ * Return: return value calculated.
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+	}
+	return (n % 10);
+}
