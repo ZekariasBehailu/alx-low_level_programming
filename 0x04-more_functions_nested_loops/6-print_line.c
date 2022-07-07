c1bf8f6dd0c270d3ec1aa6ac32f01a7f56a6d672
ZekariasBehailu <zeking02@gmail.com>
2022-07-07 17:54:53 +0300

    task 6 added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints distance to the given input
+ * @n: distance
+ *
+ */
+
+void print_line(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j < i; j++)
+			_putchar('_');
+
+		_putchar('\n');
+	}
+}
