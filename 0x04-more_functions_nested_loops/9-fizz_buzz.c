8a32d8a7a9515b72d0db8ac082964041da86a90b
ZekariasBehailu <zeking02@gmail.com>
2022-07-07 14:41:46 +0300

    task 9 fizz-buzz done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (int i = 1; i <= 100; i++)
+	{
+		if (((i / 3) == 1) && ((i / 5) == 1))
+			printf(" %s", "FizzBuzz");
+		else if ((i / 3) == 1)
+			printf(" %s", "Fizz");
+		else if ((i / 5) == 1)
+			printf(" %s", "Buzz");
+		else
+		{
+			if (i == 1)
+				printf("%d", i);
+			else
+				printf(" %d", i);
+	}
+
+	return (0);
+}
