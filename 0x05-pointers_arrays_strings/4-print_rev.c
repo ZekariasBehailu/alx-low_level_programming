b466f63f90d4936a81e2bde6949223617d68ac1c
ZekariasBehailu <zeking02@gmail.com>
2022-07-11 15:36:29 +0300

    task 4 added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - prints the reveresed string
+ * @s: string input
+ *
+ */
+
+void print_rev(char *s)
+{
+	printf((strrev(s));
+}
