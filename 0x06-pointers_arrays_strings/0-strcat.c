ead599372ac39266299b5a5c70dcb8cf29fc2982
ZekariasBehailu <zeking02@gmail.com>
2022-07-13 15:22:39 +0300

    first task done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concats two strings
+ * @dest: destination string input
+ * @src: source string input
+ *
+ * Return: returns pointer of concated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
